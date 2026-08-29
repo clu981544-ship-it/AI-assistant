c1658c8c7a8604d5841127f9618a19a95e508869
chaoming lu <clu981544@gmail.com>
2026-08-29 11:01:02 +0800

    支持多轮对话上下文

--- app/api.py
+++ app/api.py
@@ -1,28 +1,57 @@
-from fastapi import FastAPI #创建一个web api 应用
-from pydantic import BaseModel #定义前端发来的json
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
 from app.llm import chat
-app=FastAPI()#创建一个后端服务应用
-class ChatRequest(BaseModel):#规定请求的格式
-    message: str#字符串
-@app.get("/")  #get 接口 访问了就会执行root
+
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class Message(BaseModel):
+    role: str
+    content: str
+
+
+class ChatRequest(BaseModel):
+    messages: list[Message]
+
+
+@app.get("/")
 def root():
     return {
         "message": "AI Chat Assistant API"
     }
+
+
 @app.get("/health")
 def health():
-    return{
-        "status":"ok"
-    }
-@app.post("/chat")  #当客户端向 /chat 这个地址发送 POST 请求时，FastAPI 就执行下面这个函数。
-def chat_api(request: ChatRequest):
-    messages=[
-        {
-            "role":"user",
-            "content":request.message,
-        }
-    ]
-    answer=chat(messages)
     return {
-        "answer":answer
+        "status": "ok"
+    }
+
+
+@app.post("/chat")
+def chat_api(request: ChatRequest):
+    messages = []
+
+    for message in request.messages:
+        messages.append({
+            "role": message.role,
+            "content": message.content,
+        })
+
+    answer = chat(messages)
+
+    return {
+        "answer": answer
     }
